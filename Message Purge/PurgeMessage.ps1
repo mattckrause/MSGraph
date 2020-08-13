d4f3e2c7dec7adaa9c8eefb29d689456bcb9d8c1
Matt Krause <matthew.krause@outlook.com>
2020-08-12 23:32:03 -0600

    Updated with new functionality

--- Message Purge/PurgeMessage.ps1
+++ Message Purge/PurgeMessage.ps1
@@ -1,11 +1,17 @@
 <#
 Script to purge messages from mailboxes using MS Graph API/PowerShell
-Application Premissions are used for Graph Access
+Application Premissions and client credential auth flow used for Graph Access
 #>
 
+#get/set Archive parameter
+Param(
+    [bool]$archive = $false
+)
+
 #Variable declaration
-$dataFile = "C:\temp\messages.csv" # <- CSV Import File
+$dataFile = "C:\MessagePurge\messages.csv" # <- CSV Import File
 $url = "https://graph.microsoft.com/v1.0/"
+$archiveMailbox = "archivemailbox"
 
 #----FUNCTIONS----
 #Import CSV File
@@ -27,13 +33,10 @@ Function Import-Data
 #Get access token from Graph API
 Function Get-AccessToken
 {
-    $AppId = "XXXXXXXXXX" # <- Application ID from AAD App Registration
-    $AppSecret = "XXXXXXXXXX" # <- App secret from AAD App Registration
+    $AppId = "XXXXX" # <- Application ID from AAD App Registration
+    $AppSecret = "XXXXX" # <- App secret from AAD App Registration
     $Scope = "https://graph.microsoft.com/.default"
-    $Url = "https://login.microsoftonline.com/XXXXXXXXXX/oauth2/v2.0/token" # < - OAuth 2.0 token endpoint (v2) from AAD App Registration
-
-    # Add System.Web for urlencode
-    Add-Type -AssemblyName System.Web
+	$Url = "https://login.microsoftonline.com/XXXXX/oauth2/v2.0/token" # < - OAuth 2.0 token endpoint (v2) from AAD App Registration
 
     # Create body
     $Body = @{
@@ -55,7 +58,7 @@ Function Get-AccessToken
     return $Request
 }
 
-#Message Purge using Graph
+#Message Purge using Graph API
 Function MessagePurge
 {
     Param(
@@ -72,37 +75,50 @@ Function MessagePurge
                     ValueFromRemainingArguments=$false,
                     Position=1)]
             [ValidateNotNullOrEmpty()]
+            [bool]$arch,
+            [Parameter(Mandatory=$true,
+                    ValueFromPipeline=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    ValueFromRemainingArguments=$false,
+                    Position=2)]
+            [ValidateNotNullOrEmpty()]
             [String]$Token
         )
 
 	foreach ($record in $Data)
 	{
-        # Submit Request for the Token
+        #set user and message ID
         $user = $record.Recipient
         $rawmID = $record.message_id
 
         #call function to encode search string
-        write-host "Encoding URL Search String..." -ForegroundColor Green
         $mID = urlencode -ImID $rawmID
 
-        write-host "User $user"
-        Write-Host "MID $mID"
         $queryURL = $url + "users/" + $user + "/messages?`$filter=internetMessageId eq " +  "`'$($mID)`'"
-        Write-Host "The queryURL is $queryURL" -ForegroundColor Red
         $queryResponse = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Token)"} -Uri $queryURL -Method Get
-                
-        #Collect Message Info for Logging
-        $mSender = $queryResponse.value.sender.emailaddress.address
-        $mRecipient = $queryResponse.value.torecipients.emailaddress.address
-        $mSubject = $queryResponse.value.subject
-                
+        
+        #is archiving the message necessary?
+        if ($arch -eq $true)
+        {
+            #forward message to "archive" mailbox
+            $archURL = $url + "users/" + $user + "/messages/" + $queryResponse.value.ID + "/forward"
+            $archBuildBody = [ordered]@{
+                "comment" = "Forwarded as archive from message purge script.";
+                "toRecipients" = @(
+                    @{"emailAddress" = @{"address" = $archiveMailbox; "name" = "Archive Mailbox"}}
+                )
+            }
+
+            $archBody = $archBuildBody | convertto-json -Depth 3
+
+            Invoke-RestMethod -Headers @{Authorization = "Bearer $($Token)"; "Content-Type" = "application/json"} -Body $archBody -Uri $archURL -Method POST
+        }
+
         #Create URL for delete using ID collected above
         $deleteURL = $url + "users/" + $user + "/messages/" + $queryResponse.value.ID
-        Write-Host "The delete url is $deleteURL" -ForegroundColor Yellow
         #delete message
         Invoke-RestMethod -Headers @{Authorization = "Bearer $($Token)"} -Uri $deleteURL -Method Delete
         #Create message delete logging here
-        Write-Host "The message was deleted. It was From: $mSender, To: $mRecipient, with the Subject: $mSubject." -ForegroundColor Green
     }
 }
 
@@ -123,18 +139,16 @@ Function urlencode
         $b = $a[0].trim("<")
         #build encoded MID value
         $enCodeString = "<"+[uri]::EscapeDataString($b) + "@" + $a[1]
-        
         return $enCodeString 
 }
+
 #----MAIN----
 #Get User/Message Data from CSV File
 $userData = Import-Data -File $dataFile
 
 #Get OAuth Token
-write-host "Getting Token..."
 $TokenCache = Get-AccessToken
 $Token = $TokenCache.access_token
 
 #Make Requests to the Graph API to delete the messages
-Write-host "Deleting Message from Input File..."
-MessagePurge -Data $userData -Token $Token
+MessagePurge -Data $userData -arch $archive -Token $Token
--- Message Purge/ReadMe.md
+++ Message Purge/ReadMe.md
@@ -7,5 +7,8 @@ The sample reads in a list of mailboxes and messages from a .CSV file.
 
 The script is expecting Application Permissions (Client Secret/App ID). So AAD app registrations should be done accordingly.
 
+To archive messages add the archive switch and set to $true. -- purgeMessage.ps1 -Archive:$true
+
 ## Updates
 6.23.20 - Created function to encode URL in search query. This solves a bug where internet message ids that included '+' would return a null value.
+8.12.20 - Added functionality for archiving the purged messages. Controlable using the archive switch.
