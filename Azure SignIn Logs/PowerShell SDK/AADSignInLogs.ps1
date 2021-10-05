827234dcd351b0d61e797e72eed48f5dbe1efe0e
Matt Krause <matthew.krause@outlook.com>
2021-10-05 15:03:07 -0600

    change a few things around and add tested filter

--- Azure SignIn Logs/PowerShell SDK/AADSignInLogs.ps1
+++ Azure SignIn Logs/PowerShell SDK/AADSignInLogs.ps1
@@ -1,5 +1,5 @@
 ﻿<#
-Microsoft Graph PowerShell SDK Script to get aad sign-ins using Microsoft Graph API and export to .CSV.
+Microsoft Graph PowerShell SDK Script to get aad basic auth sign-ins using Microsoft Graph API and export to .CSV.
 https://docs.microsoft.com/en-us/graph/api/signin-list?view=graph-rest-1.0&tabs=http
 
 Required Permissions:AuditLog.Read.All AND Directory.Read.All (There is a know issue and currently BOTH permissions are required)
@@ -11,20 +11,16 @@ $sDate = $date.ToString("yyyy-MM-dd")
 $fileLocation = "C:\temp\temp.csv" #<- Report File Location
 
 #connecto to Microsoft Graph
-Function psmConnectToGraph
-{
-    $authCertThumb = "" #<- Certificate Thumbprint from App registration
-    $tenantID = "" #<- Tenant ID from App registration
-    $appID = "" #<- Application ID from App registration
 
-    Connect-MgGraph -ClientId $appID -TenantId $tenantID -CertificateThumbprint $authCertThumb
-}
+$authCertThumb = "" #<- Certificate Thumbprint from App registration
+$tenantID = "" #<- Tenant ID from App registration
+$appID = "" #<- Application ID from App registration
 
-#Main Script
-psmConnectToGraph
+Connect-MgGraph -ClientId $appID -TenantId $tenantID -CertificateThumbprint $authCertThumb
 
-$auditInfo = Get-MgAuditLogSignIn -Filter "CreatedDateTime ge $sDate" -All
+#request with filter for time frame and removing Modern Auth entries
+$auditInfo = Get-MgAuditLogSignIn -Filter "CreatedDateTime ge $sDate and clientAppUsed ne 'Browser' and clientAppUsed ne 'Mobile Apps and Desktop clients'" -All
 
-$auditInfo | where-object {$_.clientAppUsed -ne "Browser" -and $_.ClientAppUsed -ne "Mobile Apps and Desktop clients"} #| export-csv $fileLocation -NoTypeInformation
+$auditInfo | export-csv $fileLocation -NoTypeInformation
 
-#Disconnect-MgGraph
+Disconnect-MgGraph
