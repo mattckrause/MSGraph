b19e79ef79e64c882ca886b9f8330744caf3e928
Matt Krause <matthew.krause@outlook.com>
2021-12-07 08:56:14 -0700

    Code format updates

--- ReportNestedGroups/GetGroupMembers.ps1
+++ ReportNestedGroups/GetGroupMembers.ps1
@@ -3,35 +3,32 @@ Report all members of a group. Processing users in nested groups as well.
 Requires Microsoft Graph API PowerShell SDK (https://docs.microsoft.com/en-us/graph/powershell/installation)
 #>
 
-#Connect to Graph API using App Registration
-$appID = "" #<- Application Id from App registration
-$tenantID = "" #<- Tenant Id from App registration
-$certThumb = "" #<- Certificate Thumbpring of cert uploaded to App registration
+# Connect to Graph API using App Registration
+$appID = "" # <- Application Id from App registration
+$tenantID = "" # <- Tenant Id from App registration
+$certThumb = "" # <- Certificate Thumbpring of cert uploaded to App registration
 
 Connect-MgGraph -ClientId $appID -TenantId $tenantID -CertificateThumbprint $certThumb
 
-#variables
+# Variables
 $report = "C:\temp\GroupMembers.csv"
 $groupName = "testgroup1"
 [System.Collections.ArrayList] $output =  @()
 
-#lookup group
+# Lookup group
 $group = Get-MgGroup -Filter "DisplayName eq '$groupName'"
 
-#Get all transitive members
+# Get all transitive members
 $groupMembers = Get-MgGroupTransitiveMember -GroupId $group.Id
 
-#Add only User objects to the list for export
+# Add only User objects to the list for export
 $groupMembers | foreach-object{ `
     if($_.AdditionalProperties.'@odata.type' -like "*user") `
         {write-host $_.AdditionalProperties.displayName `
         $output.Add($_.AdditionalProperties.displayName) | Out-Null }}
 
-#Write data to .csv file
+# Write data to .csv file
 $output | Out-File $report
 
-#Print summary to console
-Write-Host $output.count "Users exported to $report"
-
-#Disconnect from Graph
-Disconnect-MgGraph #<-- Remember to disconnect from MS Graph API when the app is done!
+# Disconnect from Graph
+Disconnect-MgGraph
