6dfe8cc560e6252fea60984438dc7cdc986ceea5
Matt Krause <matt.krause@microsoft.com>
2023-02-01 16:57:04 -0700

    New Get-MessageCount.ps1 script

---
+++ MessageCount/Get-MessageCount.ps1
@@ -0,0 +1,76 @@
+﻿<#
+PS Script to use Graph API get email message count for all users in a group/dl
+requires date in format "2023-01-30"
+Matt Krause - Microsoft Graph CPX
+#>
+
+#get parameters for Script
+Param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [String]$groupName,
+    [Parameter(Mandatory=$true, Position=1)]
+    [String]$startDate,
+    [Parameter(Mandatory=$true, Position=2)]
+    [String]$endDate
+)
+
+#Variable Declaration
+$reportFile = "C:\github\MyScripts\GraphAPI\PS_SDK\Mail\MessageCount\messages.csv" # <- CSV Report
+[System.Collections.ArrayList]$MBmessageCount = @()
+$totalMessageCount = 0
+
+#----FUNCTIONS----
+#Connect to Graph using appID and certificate
+Function psmConnectToGraph
+{
+    $authCertThumb = "DB807E7E156FB01919A517971208A3EEE3CB0087"
+    $tenantID = "7b2828b9-89a3-4507-9e1a-05ff46d1192d"
+    $appID = "4b6a9db7-646c-4c4e-b083-1cce4169f424"
+
+    Connect-MGGraph -ClientId $appID -TenantId $tenantID -CertificateThumbprint $authCertThumb
+}
+
+Function Get-GroupMember
+{
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$groupName
+    )
+    $groupID = Get-MgGroup -Search "displayName:$groupName" -Select Id -ConsistencyLevel eventual
+    $groupMembers = Get-MgGroupMember -GroupId $groupID.Id
+    return $groupMembers
+}
+
+Function Count-Messages
+{
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [String]$mailbox,
+        [Parameter(Mandatory=$true, Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [String]$start,
+        [Parameter(Mandatory=$true, Position=2)]
+        [ValidateNotNullOrEmpty()]
+        [String]$end
+        )
+    $temp = Get-MgUserMessage -UserId $mailbox -Filter "ReceivedDateTime ge $start and ReceivedDateTime le $end" -Select "Id" -All -CountVariable MessageCount
+    return $MessageCount
+}
+
+#Main Script
+#authenticate
+psmConnectToGraph
+
+#Lookup Group for members and then lookup messages for each member.
+$groupUsers =  Get-GroupMember -groupName $groupName
+$groupUsers.Id | ForEach-Object {$count = Count-Messages -mailbox $_ -start (get-date $startDate).ToString('yyyy-MM-dd') -end (get-date $endDate).ToString('yyyy-MM-dd');
+    $val = [PSCustomObject]@{ 'MailboxID' = $_; 'MessageCount' = $count}
+    $totalMessageCount += $count
+    $MBmessageCount.Add($val) | out-Null; $val = $null}
+
+#
+$MBmessageCount |  Export-Csv $reportFile -NoTypeInformation
+write-host "$totalMessageCount total messages for MGDC extraction."
+Disconnect-MgGraph
---
+++ MessageCount/ReadMe.md
@@ -0,0 +1,32 @@
+﻿# Get-MessageCount.ps1
+
+## Description
+
+This is a script that uses Microsoft Graph API to report the number of messages in a group of mailboxes. Pass in a group name, and the start/end dates for collection.
+ It uses the Microsoft Graph PowerShell SDK (https://learn.microsoft.com/en-us/powershell/microsoftgraph/installation?view=graph-powershell-1.0)
+
+## Syntax for use
+
+```powershell
+Get-MessageCount.ps1 -groupName "aadGroupName" -startDate "2023-01-01" -endDate "2023-02-01"
+```
+
+## Requirements for use
+
+1. Application Registraion in Azure AD configured with certificate authentication and the following Application permissions for Microsoft Graph:
+   1. Group.Read.All
+   2. Mail.ReadBasic
+2. Update the Get-MessageCount.ps1 script to include:
+   1. Location of .CSV file report (line 17)
+   2. Your certificate thumbprint for authentication (line 25)
+   3. Your tenant ID (line 26)
+   4. Your Application ID (line 27)
+
+## Output
+
+The script will output the total count of messages in the console. It also creates a .CSV file with a message breakdown per mailbox
+[CSV Example](C:\github\MSGraph\MessageCount\messages.csv)
+
+## Disclaimer
+
+This is a work in progress
